3b8a4182cc9b6d39edfa217b0c292813a9590a6e
jonlwil <70457126+jonlwil@users.noreply.github.com>
2020-12-16 08:20:48 -0800

    Add files via upload

--- isingmax.py
+++ isingmax.py
@@ -1,17 +1,58 @@
-from random import choice
-n=8
-# Initialize all spin up case
-## Add line here from lecture notes
-# Calculate the system energy
-## Add lines here from lecture notes
-print("<E>=%4.2f"%(float(energy)/(n*n)))
-# Check this result before continuing
-#
-# Initialize random spin case (copy & edit the code given above)
-## Add line here from lecture notes
-# Calculate the system energy
-## Add lines here from lecture notes (or copy from above)
-print("<E>=%4.2f"%(float(energy)/(n*n)))
+from random import random,choice
+from math import exp
+Temp=2.3              # Temperature
+n=20                  # Sites per edge for n x n system
+n2=n*n                # Precalculate n*n
+nlist=list(range(n))  # List of sites
+ntrials=200000        # Number Trials
+nequil=100000         # Equilibration steps
 
-#Print out ising model
-## Add lines here from lecture notes
+# Initialize sums for averages
+E_sum=0.0
+E2_sum=0.0
+Emax=-5.0
+Emin=+5.0
+
+# Create initial matrix of spins all spin up
+spins=[[1 for i in range(n)] for j in range(n)]
+
+# Run simulation
+for trial in range(1,(ntrials+nequil+1)):
+    # Randomly pick a site
+    i=choice(nlist)
+    j=choice(nlist)
+
+    #Calculate the change in energy if we flip this spin
+    deltaE=2.*(spins[i][j]*\
+               (spins[i][(j+1)%n]+spins[i][(j-1+n)%n]+\
+                 spins[(i+1)%n][j]+spins[(i+-1+n)%n][j]))
+
+    #Flip the spin using Metropolis MC
+    if exp(-deltaE/Temp)>random():
+        spins[i][j]=-spins[i][j]
+    else:
+        deltaE=0.0
+
+    # Calculate system energy ONCE
+    if trial == nequil:
+        energy=0.0
+        for i in range(n):
+            for j in range(n):
+                energy-=(spins[i][j]*
+                         (spins[i][(j+1)%n]+spins[i][(j-1+n)%n]+\
+                          spins[(i+1)%n][j]+spins[(i+-1+n)%n][j]))
+        energy/=n2
+
+    # Update energy based on deltaE per spin
+    if trial > nequil:
+        energy+=2*deltaE/n2
+        if energy > Emax:
+            Emax=energy
+        E_sum+=energy
+        E2_sum+=energy*energy
+
+E_ave=E_sum/ntrials
+E2_ave=E2_sum/ntrials
+Cv=1./(Temp**2)*(E2_ave-E_ave*E_ave)
+print('%8.4f %10.6f %10.6f'%(Temp, E_ave, Cv))
+print('%10.6f %10.6f'%(Emin, Emax))
